7548a70a2d4df3727631d83640615fde31cace38
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-10-07 20:04:57 +0300

    fix mypy issues

--- starlette_babel/contrib/jinja.py
+++ starlette_babel/contrib/jinja.py
@@ -48,4 +48,4 @@ def configure_jinja_env(jinja_env: jinja2.Environment, translator: Translator | 
         }
     )
     jinja_env.add_extension("jinja2.ext.i18n")
-    jinja_env.install_gettext_translations(translator_)  # noqa
+    jinja_env.install_gettext_translations(translator_)  # type: ignore[attr-defined]
